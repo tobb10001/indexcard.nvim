b1ac82b08ca13bade9e844ea26327c907123a0a4
tobb10001 <tobb10001@gmail.com>
2023-07-14 23:45:11 +0200

    make it an actual plugin

--- lua/indexcard.lua
+++ lua/indexcard.lua
@@ -1,17 +1,57 @@
-local card = "<leader>so to source the current file"
+local M = {}
 
-local buffer = vim.api.nvim_create_buf(true, true)
-local win = vim.api.nvim_open_win(buffer, false, {
-    relative = 'editor',
-    anchor = 'SE',
-    row = vim.go.lines - 2,
-    col = vim.go.columns - 2,
-    width = 40,
-    height = 10,
-    border = 'single',
-})
+-- configuration
+local cards = {
+    {"Create cards by passing the cards option."},
+}
 
-vim.api.nvim_buf_set_lines(buffer, 1, 1, false, {card})
+-- runtime vars
+local buffer = nil
+local win = nil
 
-vim.api.nvim_win_set_option(win, 'number', false)
-vim.api.nvim_win_set_option(win, 'rnu', false)
+function set_buffer()
+    buffer = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_buf_set_lines(buffer, 1, 1, false, cards[1])
+end
+
+function M.show()
+    if win ~= nil then return end
+
+    if buffer == nil then
+        set_buffer()
+    end
+
+    win = vim.api.nvim_open_win(buffer, false, {
+        relative = 'editor',
+        anchor = 'SE',
+        row = vim.go.lines - 2,
+        col = vim.go.columns - 2,
+        width = 40,
+        height = 10,
+        border = 'single',
+    })
+    vim.api.nvim_win_set_option(win, 'number', false)
+    vim.api.nvim_win_set_option(win, 'rnu', false)
+end
+
+function M.hide()
+    if win == nil then return end
+
+    vim.api.nvim_win_hide(win)
+end
+
+function M.toggle()
+    if win == nil then
+        M.show()
+    else
+        M.hide()
+    end
+end
+
+function M.setup(opts)
+    if opts.cards ~= nil then
+        cards = opts.cards
+    end
+end
+
+return M
